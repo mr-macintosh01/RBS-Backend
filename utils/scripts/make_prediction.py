2eb6204d40adcae2727f9d6c3627f7503bb421d0
mr-macintosh01 <mr.macintosh01@gmail.com>
2024-11-26 13:04:46 +0100

    Changed use of the new model extension

--- utils/scripts/make_prediction.py
+++ utils/scripts/make_prediction.py
@@ -4,7 +4,7 @@ import sys
 import joblib
 
 scaler = joblib.load('./utils/scripts/model/scaler/scaler.gz')
-model = tf.keras.models.load_model('./utils/scripts/model/first_model/Dense_Model.keras')
+model = tf.keras.models.load_model('./utils/scripts/model/first_model/Dense_Model.h5')
 
 scaled_vector = scaler.transform([sys.argv[1:]])
 predicted_value = model.predict(scaled_vector, verbose=0).flatten()
