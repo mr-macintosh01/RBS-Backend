c17d8adcb31d7a150382437169a3aae0d0950483
mr-macintosh01 <mr.macintosh01@gmail.com>
2024-12-09 19:46:15 +0100

    New CORS test

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.json())
 
 app.use(
     cors({
-        origin: `${process.env.REACT_APP_FRONT_URL}`,
+        origin: [`${process.env.REACT_APP_FRONT_URL}`],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         exposedHeaders: 'verificationStatus',
         allowedHeaders: ['Content-Type', 'passwordhash'],
