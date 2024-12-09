218179a73f1ea304c4df08ec00029a8d3d9d31c1
mr-macintosh01 <mr.macintosh01@gmail.com>
2024-12-09 18:45:26 +0100

    Added all CORS

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.json())
 
 app.use(
     cors({
-        origin: [process.env.REACT_APP_FRONT_PORT],
+        origin: ['*'],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         exposedHeaders: 'verificationStatus',
         allowedHeaders: ['Content-Type', 'passwordhash'],
